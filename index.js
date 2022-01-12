203b24266aebf315375da19eccbbf5c3157e21f1
Skruphold <hunterco999@gmail.com>
2022-01-12 16:31:13 -0600

    added inquirer prompt and got a response in terminal

--- index.js
+++ index.js
@@ -1,7 +1,57 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
+const markDown = require('./utils/generateMarkdown')
 
 // TODO: Create an array of questions for user input
-const questions = [];
+// const questions = [];
+inquirer.prompt([
+    {
+        type: "input",
+        message: "would is the Title?",
+        name: "title"
+    },
+    {
+        type: "input",
+        message: "What is your description?",
+        name: "description"
+    },
+    {
+        type: "input",
+        message: "Table of contents?",
+        name: "contents"
+    },
+    {
+        type: "list",
+        message: "Which license?",
+        name: "license",
+        choices: ["Apache License 2.0", "BSD License", "GNU License", "MIT License", "Mozilla public License 2.0"]
+    },
+    {
+        type: "input",
+        message: "Installment instructions?",
+        name: "install"
+    },
+    {
+        type: "input",
+        message: "Usage information?",
+        name: "usage"
+    },
+    {
+        type: "input",
+        message: "Contribution?",
+        name: "contribution"
+    },
+    {
+        type: "input",
+        message: "Test instructions?",
+        name: "testing"
+    }
+]).then((answers) => {
+    console.log(answers);
+    var ReadmeStr = generateMarkdown(answers);
+})
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
